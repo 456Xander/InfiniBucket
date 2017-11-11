8c6fd0c39a285f132a51bf964a74247080023797
Alex Daum <daumalex10200@gmail.com>
2017-11-11 10:41:29 +0100

    Initial Commit

---
+++ src/main/java/at/xander/infinibucket/main/InfiniBucket.java
@@ -0,0 +1,8 @@
+package at.xander.infinibucket.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid=InfiniBucket.MODID, name="Infini Bucket", version="1.0.0")
+public class InfiniBucket {
+	public static final String MODID = "infinibucket";
+}
